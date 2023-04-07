84cfd56ca239c1753eebec98283551e3d7db1e4f
unknown <asharabiani@instructors.2u.com>
2023-04-07 11:10:15 -0400

    update info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Team from './components/team';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+
+      <p>
+        Welcome to [company name], where we believe that psychedelics have the power to transform individuals, communities, and society as a whole. Our mission is to create a safe and welcoming space for the psychedelic community to connect, learn, and grow.
+      </p>
+      <p>
+        At [company name], we offer a mobile application that provides information on upcoming psychedelic events and allows users to connect with like-minded individuals. Whether you're interested in attending a local psychedelic ceremony, participating in a group meditation, or simply learning more about the psychedelic experience, our app has you covered.
+      </p>
+      <p>
+        Our team is made up of experienced professionals who are passionate about psychedelics and their potential for healing and personal growth. We are dedicated to providing accurate and up-to-date information about psychedelic events and resources, as well as creating a supportive community for all who are interested in exploring psychedelics.
+      </p>
+      <p>
+        Thank you for considering [company name] for your psychedelic needs. We are excited to be a part of your journey towards self-discovery and personal transformation.
+      </p>
+      <Team />
+    </div >
   );
 }
 
---
+++ src/components/Team.jsx
@@ -0,0 +1,19 @@
+export default function Team() {
+  return (<>
+    <h1>Meet the Team</h1>
+    <p>
+      At [company name], our team is made up of passionate and experienced professionals who are dedicated to supporting the psychedelic community. Here are a few of the people who make our mission possible:
+    </p>
+    <p>
+      Dr. Shahin Vakilinia, CEO and Co-Founder: Dr. Vakilinia is a licensed psychologist and experienced researcher who specializes in the therapeutic use of psychedelics. He is committed to advancing the field of psychedelic therapy and creating a safe and supportive community for all who are interested in exploring psychedelics.
+    </p>
+    Ali Sharabiani, CTO and Co-Founder: Ali is a skilled software developer and entrepreneur who is passionate about using technology to support positive social change. He has extensive experience in building mobile applications and is dedicated to creating a user-friendly and engaging platform for the psychedelic community.
+    <p>
+      Mehdi Maghrouni, Community Manager: Mehdi is a community builder and activist who is passionate about creating safe and inclusive spaces for marginalized groups. He is committed to fostering a welcoming and supportive community for all who are interested in psychedelics, regardless of their background or experience level.
+    </p>
+    <p>
+      Together, our team is dedicated to supporting the psychedelic community and advancing the field of psychedelic therapy. We are excited to be a part of this growing movement and look forward to connecting with you.
+    </p>
+  </>
+  );
+}
