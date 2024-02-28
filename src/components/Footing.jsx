018ab1119285822d15b040d0961acf0b84e56934
alisha-monark <ali.sharabiani@monark.com>
2024-02-27 20:49:57 -0500

    Update the footer animation

--- src/components/Footing.jsx
+++ src/components/Footing.jsx
@@ -3,14 +3,24 @@ import { faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free
 
 
 export default function Footing({ companyName }) {
+  const icons = [
+    { icon: faYoutube, link: '[insert youtube link]' },
+    { icon: faFacebook, link: '[insert facebook link]' },
+    { icon: faTwitter, link: '[insert twitter link]' },
+    { icon: faInstagram, link: '[insert instagram link]' },
+  ];
+
   return (
     <footer>
-      <ul className="social-icons" style={{ padding: '0' }}>
-        <li><a className="link" href="[insert social media link]"><FontAwesomeIcon icon={faYoutube} beat /></a></li>
-        <li><a className="link" href="[insert social media link]"><FontAwesomeIcon icon={faFacebook} beat /></a></li>
-        <li><a className="link" href="[insert social media link]"><FontAwesomeIcon icon={faTwitter} beat /></a></li>
-        <li><a className="link" href="[insert social media link]"><FontAwesomeIcon icon={faInstagram} color='#fba161' beat /></a></li>
-      </ul>
+     <ul className="social-icons" style={{ padding: '0' }}>
+      {icons.map((social, index) => (
+        <li key={index} >
+          <a className="link" href={social.link}>
+            <FontAwesomeIcon icon={social.icon} beat style={{ animationDelay: `${index * 0.2}s`, animationDuration: '2s' }}/>
+          </a>
+        </li>
+      ))}
+    </ul>
 
       <div >
         <p>Copyright © {(new Date()).getFullYear()} {companyName}.
