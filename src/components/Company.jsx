d8a8335a736c3be09b8961692d1730bcce4ef52e
alisha-monark <ali.sharabiani@monark.com>
2024-02-27 20:08:47 -0500

    Updated the site content

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -2,16 +2,16 @@ export default function Company({ companyName }) {
   return (<>
     <h1 id="about">About</h1>
     <p>
-      Welcome to {companyName}, where we believe that psychedelics have the power to transform individuals, communities, and society as a whole. Our mission is to create a safe and welcoming space for the psychedelic community to connect, learn, and grow.
+      Welcome to {companyName}, here we introduce a groundbreaking spiritual AI companion designed to elevate your journey towards self-discovery and personal transformation. While our focus is on the symbiosis of spirituality and advanced AI, we remain committed to fostering connection, learning, and growth in an inclusive environment
     </p>
     <p>
       At {companyName}, we offer a mobile application that provides information on upcoming psychedelic events and allows users to connect with like-minded individuals. Whether you're interested in attending a local psychedelic ceremony, participating in a group meditation, or simply learning more about the psychedelic experience, our app has you covered.
     </p>
     <p>
-      Our team is made up of experienced professionals who are passionate about psychedelics and their potential for healing and personal growth. We are dedicated to providing accurate and up-to-date information about psychedelic events and resources, as well as creating a supportive community for all who are interested in exploring psychedelics.
+      Our team is composed of dedicated professionals passionate about the intersection of spirituality and artificial intelligence. With a profound belief in the potential for healing and personal growth, we are committed to offering accurate and up-to-date information about spiritual events, resources, and creating a supportive community for all seekers.
     </p>
     <p>
-      Thank you for considering {companyName} for your psychedelic needs. We are excited to be a part of your journey towards self-discovery and personal transformation.
+      Thank you for considering {companyName} as your partner in spiritual exploration. We are excited to be a part of your quest for deeper understanding and personal evolution through the harmonious integration of spirituality and advanced AI technology.
     </p>
   </>);
 }
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -9,16 +9,16 @@ export default function Team({ companyName }) {
     </p>
     <img className="team-profile-pic" src={shahin} width={50} />
     <p>
-      Dr. Shahin Vakilinia, CEO and Co-Founder: Dr. Vakilinia is a licensed psychologist and experienced researcher who specializes in the therapeutic use of psychedelics. He is committed to advancing the field of psychedelic therapy and creating a safe and supportive community for all who are interested in exploring psychedelics.
+      Dr. Shahin Vakilinia, CEO and Co-Founder: our esteemed CEO and visionary leader at ArchangX. Shahin's dynamic background and extensive experience underscore his unique ability to fuse technical proficiency with a profound understanding of spirituality, steering our company towards transformative innovations.
+    </p>
+    <p>
+      Shahin Vakilinia, an accomplished professional in artificial intelligence and software development, consistently showcases a commitment to pioneering solutions that leverage technology for personal growth and spiritual exploration. His leadership is rooted in a deep conviction of the harmonious integration of advanced AI and spirituality, positioning ArchangX at the forefront of redefining self-discovery.
     </p>
     <img className="team-profile-pic" src={ali} width={50} />
     <p>
-      Ali Sharabiani, CTO and Co-Founder: Ali is a skilled software developer and entrepreneur who is passionate about using technology to support positive social change. He has extensive experience in building mobile applications and is dedicated to creating a user-friendly and engaging platform for the psychedelic community.
+      Ali Sharabiani, CTO and Co-Founder: Ali is a skilled software developer and entrepreneur who is passionate about using technology to support positive social change. He has extensive experience in building mobile applications and is dedicated to creating a user-friendly and engaging platform for the community.
     </p>
-    
-    <p>
-      Together, our team is dedicated to supporting the psychedelic community and advancing the field of psychedelic therapy. We are excited to be a part of this growing movement and look forward to connecting with you.
-    </p>
+       
   </>
   );
 }
