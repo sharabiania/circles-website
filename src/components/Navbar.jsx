663f460036fef23b7fddad279484677646e05a05
alisha-monark <ali.sharabiani@monark.com>
2024-02-27 20:41:41 -0500

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const logo = require('../assets/logo.png');
 export default function Navbar() {
   return (
     <nav>
-      <img src={logo} width={100} height={100} />
+      <img src={logo} width={150} height={150} />
       <ul>
         <li><a className='anchor' href="#about">About</a></li>
         <li><a className='anchor' href="#team">Team</a></li>
